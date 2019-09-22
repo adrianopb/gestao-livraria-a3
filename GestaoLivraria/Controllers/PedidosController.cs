ce0e212b8798e404f1e8a88a0462659db4f2fd6b
adrianopb <adrianopbraga1@gmail.com>
2019-09-22 18:39:04 -0300

    Commit inicial

---
+++ GestaoLivraria/Controllers/PedidosController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GestaoLivraria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoLivraria.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        // GET api/livros
+        [HttpGet]
+        //        public ActionResult<IEnumerable<Livro>> Get()
+        //        {
+        //            Livro v_Livro = new Livro();
+        //            
+        //            List<Livro> v_
+        //            
+        //            return new string[] { "value1", "value2" };
+        //        }
+
+        // GET api/livros/5
+        [HttpGet("{id}")]
+        public IEnumerable<Livro> Get(int? id)
+        {
+            Livro v_Livro = new Livro();
+            IEnumerable<Livro> v_ListaLivros = new List<Livro>();
+
+            v_ListaLivros = v_Livro.BuscarLivros(id);
+
+            //if (v_ListaLivros == null)
+            //{
+                //falta gerar a exceção
+            //}
+
+            return v_ListaLivros;
+        }
+
+        // POST api/livros
+        [HttpPost]
+        public Livro Post([Bind("Id,Nome")] Livro value)
+        {
+            //if (value.Id > 100)
+            //{
+            //  falta gerar a exceção
+            //}
+
+            return new Livro()
+            {
+                Id = value.Id,
+                Nome = value.Nome
+            };
+        }
+
+        // PUT api/livros/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/livros/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GestaoLivraria/Models/Livro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoLivraria.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public IEnumerable<Livro> BuscarLivros(int? id)
+        {
+            var v_ListaLivros = new List<Livro>(ListaLivros());
+
+            if (id != null)
+            {
+                return v_ListaLivros.Any(q => q.Id == id) ? v_ListaLivros.Where(q => q.Id == id) : null;
+            }
+
+            return v_ListaLivros;
+        }
+
+        public IEnumerable<Livro> ListaLivros()
+        {
+            var v_ListaLivros = new List<Livro>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                var v_Livro = new Livro()
+                {
+                    Id = i,
+                    Nome = "Livro " + i,
+                };
+
+                v_ListaLivros.Add(v_Livro);
+            }
+
+            return v_ListaLivros;
+        }
+    }
+}
